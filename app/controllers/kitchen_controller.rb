3b36f1c7b18ff54a80c27c8006b401cc2d8c0e3e
a <a>
2017-11-17 17:30:58 -0600

    Kitchen con refresh despichado

--- app/controllers/kitchen_controller.rb
+++ app/controllers/kitchen_controller.rb
@@ -3,6 +3,7 @@ class KitchenController < ApplicationController
 
   def index
   	#@orders = Order.select("product_id, quantity, table_id, date_time, comment").where('product_id <> NULL AND status <> 2').order(:date_time)
-  	@orders = Order.find_by_sql(["SELECT * FROM ORDERS WHERE PRODUCT_ID IN (SELECT PRODUCT_ID FROM PRODUCTS WHERE CATEGORY_ID IN (SELECT ID FROM CATEGORIES WHERE IS_BAR = 'f') )"])
+  	#@orders = Order.all
+  	@orders = Order.find_by_sql(" SELECT * FROM orders WHERE status = 1 ORDER BY created_at ASC ")
   end
 end
